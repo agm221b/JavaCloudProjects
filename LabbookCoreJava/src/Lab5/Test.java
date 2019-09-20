97a7689be65ca68e9d8b7555728d673b026b29a5
agm221b <adityamishr123@gmail.com>
2019-09-20 12:37:56 +0530

    added labbook core java

---
+++ LabbookCoreJava/src/Lab5/Test.java
@@ -0,0 +1,24 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		int ch = sc.nextInt();
+		
+		switch(ch)
+		{
+		case 1:
+			
+			break;
+		
+		case 2: 
+		}
+	}
+
+}
